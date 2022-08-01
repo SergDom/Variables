ac5c5fd47be7fdca5c861a9f222ea1c42a825283
sergeydomanov <nomad-labs@yandex.ru>
2022-08-01 20:47:54 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+           //Цельночисленные
+        byte apple = 5;
+        short pearl = 11;
+        int orange = 21;
+        long grapes = 45L;
+
+           //С плавующей точкой
+        float sand = 2.1F;
+        double pound = 0.454;
+
+           //Символы
+        char star = 42;
+        System.out.println(star);
+
+           //Логические
+        int child = 1;
+        boolean schoolClass = child > 20;
+        System.out.println(schoolClass);
+    }
+}
